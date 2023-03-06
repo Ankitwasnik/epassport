ec17f657d02d539ec69387fb292e6ebfb8f291fb
Ankit Wasnik <ankitwasnik7@gmail.com>
2023-03-06 19:05:46 +0530

    Init

---
+++ lib.rs
@@ -0,0 +1,84 @@
+#![cfg_attr(not(feature = "std"), no_std)]
+
+
+#[ink::contract]
+mod epassport {
+
+    /*
+        Functions:
+        1. Store a certificate from CSCA
+            - Store country wise certificates
+        2. Store DSO and DSC
+        3. Vaidate DSO and DSC
+        4. Get a certificate
+        5. Get DSO and DSC
+    */
+
+    /// Defines the storage of your contract.
+    /// Add new fields to the below struct in order
+    /// to add new static storage fields to your contract.
+    #[ink(storage)]
+    pub struct Epassport {
+        /// Stores a single `bool` value on the storage.
+        value: bool
+    }
+
+    impl Epassport {
+        /// Constructor that initializes the `bool` value to the given `init_value`.
+        #[ink(constructor)]
+        pub fn new(init_value: bool) -> Self {
+            Self { value: init_value }
+        }
+
+        /// Constructor that initializes the `bool` value to `false`.
+        ///
+        /// Constructors can delegate to other constructors.
+        #[ink(constructor)]
+        pub fn default() -> Self {
+            Self::new(Default::default())
+        }
+
+        /// A message that can be called on instantiated contracts.
+        /// This one flips the value of the stored `bool` from `true`
+        /// to `false` and vice versa.
+        #[ink(message)]
+        pub fn flip(&mut self) {
+            self.value = !self.value;
+        }
+
+        /// Simply returns the current value of our `bool`.
+        #[ink(message)]
+        pub fn get(&self) -> bool {
+            self.value
+        }
+
+       
+    }
+
+    /// Unit tests in Rust are normally defined within such a `#[cfg(test)]`
+    /// module and test functions are marked with a `#[test]` attribute.
+    /// The below code is technically just normal Rust code.
+    #[cfg(test)]
+    mod tests {
+        /// Imports all the definitions from the outer scope so we can use them here.
+        use super::*;
+
+        /// We test if the default constructor does its job.
+        #[ink::test]
+        fn default_works() {
+            let epassport = Epassport::default();
+            assert_eq!(epassport.get(), false);
+        }
+
+        /// We test a simple use case of our contract.
+        #[ink::test]
+        fn it_works() {
+            let mut epassport = Epassport::new(false);
+            assert_eq!(epassport.get(), false);
+            epassport.flip();
+            assert_eq!(epassport.get(), true);
+        }
+
+        
+    }
+}
